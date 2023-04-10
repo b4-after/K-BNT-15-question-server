728d29e9863dba4a3733a7478c7e55b89d742c4f
does <ldk980130@gmail.com>
2023-04-11 00:07:28 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/b4after/bntquestion/BntQuestionApplication.java
@@ -0,0 +1,13 @@
+package com.b4after.bntquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BntQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BntQuestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b4after/bntquestion/BntQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b4after.bntquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BntQuestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
