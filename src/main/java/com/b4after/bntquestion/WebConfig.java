a14da745dc4c475543c45c54dc796ec5d510e64c
does <ldk980130@gmail.com>
2023-05-04 15:44:32 +0900

    chore: cors 설정

---
+++ src/main/java/com/b4after/bntquestion/WebConfig.java
@@ -0,0 +1,16 @@
+package com.b4after.bntquestion;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
